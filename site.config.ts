8e44e48d26d5f5f418f9cb54d723bf734d16d6c3
ridwan <ridwan.am.pm@gmail.com>
2024-10-03 11:10:09 +0700

    -config

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
-
-  // if you want to restrict pages to a single notion workspace (optional)
+  rootNotionPageId: '24c1468a58d04599b27162e818f1b342',
+ // https://www.notion.so/Keranjang-Belanja-24c1468a58d04599b27162e818f1b342
+// if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
+  // .
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Keranjang Ku',
+  domain: 'nextjs-notion-mu.vercel.app',
+  author: 'Ridwan AM',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Webiste Kentajang Ku',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '/',
+  github: '/',
+  linkedin: '/',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
